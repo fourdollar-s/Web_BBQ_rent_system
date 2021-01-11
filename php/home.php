64a78eb7494d5604afec50f8e1ee29cb9ea826a8
fourdollar <61176848+fourdollar-s@users.noreply.github.com>
2021-01-11 19:19:59 +0800

    Create home.php

---
+++ php/home.php
@@ -0,0 +1,58 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<style>
+			<?php include "home.css" ;?>
+		</style>
+		<title>Home</title>
+	</head>
+<body>
+		<!-- 網頁最上方的標題 -->
+		<header>
+			<h1><a href="home.php">高雄大學露營烤肉區租借系統</a></h1>
+		</header>
+		
+		<div class="box">
+        		<div class="wrap">
+            		<a href="status_inquire.php">場地狀態查詢</a>
+            		<a href="price_display.php">價位查詢</a>
+            		<a href="login.php">使用者登入</a>
+        	</div>
+
+
+		<div class="content">
+			<div class="content1">
+				<h1></br></h1>
+				<h1>背景介紹</br></h1>
+				<h2>目前在高雄大學如需租借露營烤肉場地，租借申請人仍須按流程至各處室進行實體紙本申請，無法在網路上完成所有流程，造成各處室與租借申請人在申請時，可能有資訊不對稱、責任歸屬不明、申請過程耗時的問題，此項專題的目的在於使租借露營烤肉場地的流程系統化，並能透過資料庫對系統進行有效管理。</h2>
+			</div>
+
+			<div class="content2">
+				<h1></br></h1>
+				<h1>系統功能</h1>
+				<h2>◆申請帳號</h2>
+				<h2>◆編輯帳號基本資料</h2>
+				<h2>◆查詢歷史租借紀錄</h2>
+				<h2>◆審核狀態查詢</h2>
+				<h2>◆申請租借場地</h2>
+			</div>
+
+			<div class="content3">
+			<h1></h1>
+			</div>
+		</div>
+
+
+		<!-- 網頁下方的工具列或訊息 -->
+		<footer>
+			<h1>
+				© 2015 高雄大學 National University of Kaohsiung<br>
+				81148 高雄市楠梓區高雄大學路700號<br>
+				700, Kaohsiung University Rd., Nanzih District, Kaohsiung 811, Taiwan, R.O.C.<br>
+				高大總機:886-7-5919000 傳真號碼:886-7-5919083<br>
+				高大校園緊急聯絡電話:886-7-5917885 高大警衛室:886-7-5919009<br>
+			</h1>
+		</footer>
+</body>
+</html>
